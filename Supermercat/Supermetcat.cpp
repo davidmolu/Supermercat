17d200a8b6f43c9a3a91f87a72efbd22bee7a2ff
Álvaro Girona <alvaro.girona@gmail.com>
2015-11-08 16:08:30 +0100

    .

---
+++ Supermercat/Supermetcat.cpp
@@ -0,0 +1,9 @@
+//
+//  Supermetcat.cpp
+//  Supermercat
+//
+//  Created by Álvaro Girona Arias on 8/11/15.
+//  Copyright © 2015 Álvaro Girona. All rights reserved.
+//
+
+#include "Supermetcat.hpp"
---
+++ Supermercat/Supermetcat.hpp
@@ -0,0 +1,14 @@
+//
+//  Supermetcat.hpp
+//  Supermercat
+//
+//  Created by Álvaro Girona Arias on 8/11/15.
+//  Copyright © 2015 Álvaro Girona. All rights reserved.
+//
+
+#ifndef Supermetcat_hpp
+#define Supermetcat_hpp
+
+#include <stdio.h>
+
+#endif /* Supermetcat_hpp */
